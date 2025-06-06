a2eec80ae5b42109e062d2ebdedca845af2c2a52
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-06-06 08:30:54 -0400

    06/06/2026 1.2

--- src/routes/03/dummy/users/+page.server.js
+++ src/routes/03/dummy/users/+page.server.js
@@ -1,5 +1,12 @@
-export async function load({ fetch }) {
-    const res = await fetch('https://dummyjson.com/users');
+export async function load({ fetch, url }) {
+    const pais = url.searchParams.get('pais');
+    const link = 'https://dummyjson.com/users';
+
+    if (pais) {
+      link += `/filter?key=address.country&value=${pais}`;
+    }
+
+    const res = await fetch({'https://dummyjson.com/users'});
     const users = await res.json();
     return {users}; 
   }
--- src/routes/03/dummy/users/+page.svelte
+++ src/routes/03/dummy/users/+page.svelte
@@ -2,6 +2,13 @@
     let { data } = $props();
 </script>
 <h1>Usuários:</h1>
+
+<form>
+    <input name='pais' type='text' placeholder='Filtrar por país' class="form-control mb-3" />
+    <button>FIltrar</button>
+</form>
+
+
 {#each data.users.users as user}
     <li><a href="/03/dummy/users/{user.id}">{user.firstName} - {user.address.country}</a></li>
 {/each}
