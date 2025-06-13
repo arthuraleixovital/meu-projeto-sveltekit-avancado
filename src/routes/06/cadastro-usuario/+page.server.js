43ca594da91063993a8af124c3f55e09f2b57b63
arthuraleixovital <arthuraleixovital@outlook.com.br>
2025-06-13 08:29:41 -0400

    Aula 13/06/2025

--- src/routes/06/cadastro-usuario/+page.server.js
+++ src/routes/06/cadastro-usuario/+page.server.js
@@ -1,22 +1,49 @@
 import { fail, redirect } from '@sveltejs/kit';
- 
+
+//** retorna true se o texto contem pelo menos um dos caracteres */
+function contem(texto, caracteres){
+    for (const caractere of caracteres)
+      if(texto.includes(caractere)) return true;
+    return false;
+}
+
 export const actions = {
   default: async ({ request }) => {
     const data = await request.formData();
-    const nome = data.get('nome');
-    const email = data.get('email');
-    const nascimento = data.get('nascimento');
- 
-    if (!email || !nome || !nascimento) return fail(400, { error: 'Email e senha são obrigatórios.', email, nome, nascimento });
- 
-    if (!email.includes('@')) return fail(400, { error: 'Email inválido.', email });
- 
-    if (email == "email@inexistente") return fail(400, { error: 'Email ou senha inválidos.', email });
+    const dados={
+      nome: data.get('nome'),
+      senha: data.get('senha'),
+      confirmacaoSenha: data.get('confirmacaosenha'),
+      email: data.get('email'),
+      nascimento: data.get('nascimento'),
+      erros: []
+    }
 
-    let agora= new Date();
-    let nascimentoDate = new Date(nascimento);
-    if (agora - nascimentoDate < 378691200000) return fail(400, { error: 'Você ainda não completou 12 anos!', email, nome, nascimento });
- 
+    if (!dados.email || !dados.nome || !dados.nascimento || !dados.senha || !dados.confirmacaoSenha) 
+      dados.erros[0]=('Preencha todos os campos.');
+
+    if (!dados.email.includes('@')) 
+      dados.erros[1]=('O e-mail deve conter um @.');
+
+    if (
+      !contem(dados.senha, 'abcdefghijklmnopqrstuvwxyz') ||
+      !contem(dados.senha, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') ||
+      !contem(dados.senha, '0123456789') ||
+      !contem(dados.senha, '!@#$%¨&*()-_+=')
+    )
+      dados.erros[2]=(
+        'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'
+      );
+
+      if (dados.senha != dados.confirmacaoSenha) 
+        dados.erros[3]=('A senha e a confirmação de senha não coincidem.');
+
+    let agora = new Date();
+    let nascimentoDate = new Date(dados.nascimento);
+    if (agora - nascimentoDate < 378691200000)
+      dados.erros[4]=('Você deve ter pelo menos 12 anos para se cadastrar.');
+
+    if (dados.erros.length > 0) return fail(400, dados);
     redirect(303, '/06/profile');
   }
 };
